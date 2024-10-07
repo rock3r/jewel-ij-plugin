efc6617a0f61c91394237d8951b679add63bc836
Sebastiano Poggi <sebp@google.com>
2024-10-07 17:10:48 +0200

    Create custom plugin repository to store releases
    Will be available at https://raw.githubusercontent.com/rock3r/jewel-ij-plugin/refs/heads/main/plugin-repository/updatePlugins.xml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,16 @@
+import dev.bmac.gradle.intellij.UpdateXmlTask
+import dev.bmac.gradle.intellij.UploadPluginTask
 import org.jetbrains.changelog.Changelog
+import org.jetbrains.changelog.ChangelogPluginExtension
 import org.jetbrains.changelog.markdownToHTML
+import org.jetbrains.intellij.platform.gradle.tasks.BuildPluginTask
 
 plugins {
   id("java")
   alias(libs.plugins.kotlin)
   alias(libs.plugins.intelliJPlatform)
   alias(libs.plugins.changelog)
+  alias(libs.plugins.pluginUploader)
 }
 
 group = providers.gradleProperty("pluginGroup").get()
@@ -52,40 +57,42 @@ dependencies {
   }
 }
 
+fun readPluginDescription() =
+  providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
+    val start = "<!-- Plugin description -->"
+    val end = "<!-- Plugin description end -->"
+
+    with(it.lines()) {
+      if (!containsAll(listOf(start, end))) {
+        throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+      }
+      subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
+    }
+  }
+
+fun readPluginChangeNotes(changelog: ChangelogPluginExtension) =
+  providers.gradleProperty("pluginVersion").map { pluginVersion ->
+    with(changelog) {
+      renderItem(
+        (getOrNull(pluginVersion) ?: getUnreleased())
+          .withHeader(false)
+          .withEmptySections(false),
+        Changelog.OutputType.HTML,
+      )
+    }
+  }
+
 intellijPlatform {
   pluginConfiguration {
     version = providers.gradleProperty("pluginVersion")
 
     // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's
     // manifest
-    description =
-      providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
-        val start = "<!-- Plugin description -->"
-        val end = "<!-- Plugin description end -->"
-
-        with(it.lines()) {
-          if (!containsAll(listOf(start, end))) {
-            throw GradleException(
-              "Plugin description section not found in README.md:\n$start ... $end"
-            )
-          }
-          subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
-        }
-      }
+    description = readPluginDescription()
 
     val changelog = project.changelog // local variable for configuration cache compatibility
     // Get the latest available change notes from the changelog file
-    changeNotes =
-      providers.gradleProperty("pluginVersion").map { pluginVersion ->
-        with(changelog) {
-          renderItem(
-            (getOrNull(pluginVersion) ?: getUnreleased())
-              .withHeader(false)
-              .withEmptySections(false),
-            Changelog.OutputType.HTML,
-          )
-        }
-      }
+    changeNotes = readPluginChangeNotes(changelog)
 
     ideaVersion {
       sinceBuild = providers.gradleProperty("pluginSinceBuild")
@@ -123,6 +130,35 @@ tasks {
   wrapper { gradleVersion = providers.gradleProperty("gradleVersion").get() }
 
   publishPlugin { dependsOn(patchChangelog) }
+
+  named("generateBlockMap") { dependsOn(named("buildPlugin")) }
+
+  create<Copy>("copyPluginZipToLocalRepository") {
+    dependsOn(named("buildPlugin"))
+
+    val pluginZipFile = (named("buildPlugin").get() as BuildPluginTask).outputs.files.singleFile
+    from(pluginZipFile)
+
+    into(file("plugin-repository/"))
+  }
+
+  create<UpdateXmlTask>("updateLocalPluginRepository") {
+    dependsOn(named("copyPluginZipToLocalRepository"))
+    val pluginFile = (named("copyPluginZipToLocalRepository").get() as Copy).outputs.files.singleFile
+
+    updateFile.set(file("plugin-repository/updatePlugins.xml"))
+    downloadUrl =
+      "https://raw.githubusercontent.com/rock3r/jewel-ij-plugin/refs/heads/main/plugin-repository/${pluginFile.name}"
+    pluginName = "Jewel"
+    pluginId = providers.gradleProperty("pluginGroup")
+    version = providers.gradleProperty("pluginVersion")
+    sinceBuild = providers.gradleProperty("pluginSinceBuild")
+    untilBuild = providers.gradleProperty("pluginUntilBuild")
+    pluginDescription = readPluginDescription()
+
+    val changelog = project.changelog // local variable for configuration cache compatibility
+    changeNotes = readPluginChangeNotes(changelog)
+  }
 }
 
 apply(from = "sync-files.gradle.kts")
